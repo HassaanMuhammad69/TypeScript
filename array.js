ebac1c26e716c79f40e12487a1c929191b34c20e
HASSAAN MUHAMMAD <hassaans299@gmail.com>
2023-10-19 02:02:40 +0500

    array

---
+++ array.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHero = [];
+superHero.push("spidey");
+var user = [];
+user.push({ name: "John", isPaid: true });
---
+++ array.ts
@@ -0,0 +1,13 @@
+const superHero: string[] =[]
+
+superHero.push("spidey")
+
+type User={
+    name:string
+    isPaid: true
+}
+
+const user :User[]= []
+user.push({name:"John",isPaid:true})
+
+export{}
