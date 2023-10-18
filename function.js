eec2cd6d5ba72aaacf78cf6cdfa44751f0d72b61
HASSAAN MUHAMMAD <hassaans299@gmail.com>
2023-10-18 16:14:56 +0500

    function is done

--- function.js
+++ function.js
@@ -14,3 +14,21 @@ addtwo(5);
 getUpper("hassaan");
 SignupUser("hassaan", "hassan@gam.com", false);
 loginUser("Hassaan", "hassan@gmail.com");
+///Better way to write function
+var getHello = function (myval) {
+    return "";
+};
+getHello("Hassan");
+//Map
+var heroes = ["thor", "spidey"];
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+//When to use void
+function consoleERR(errMsg) {
+    console.log(errMsg);
+}
+//When to use Never
+function handleErr(errMsg) {
+    throw new Error(errMsg);
+}
--- function.ts
+++ function.ts
@@ -16,6 +16,29 @@ getUpper("hassaan")
 SignupUser("hassaan", "hassan@gam.com",false)
 loginUser("Hassaan","hassan@gmail.com")
 
+///Better way to write function
+const getHello= (myval:string):string =>{
+    return ""
+} 
+getHello("Hassan")
 
+
+
+//Map
+const heroes=["thor", "spidey"]
+
+heroes.map((hero):string =>{
+    return `hero is ${hero}` 
+})
+
+//When to use void
+function consoleERR(errMsg:string): void{
+     console.log(errMsg)
+}
+
+//When to use Never
+function handleErr(errMsg:string): never{
+    throw new Error(errMsg)
+}
 
 export{}
